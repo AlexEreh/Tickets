20f02f3e3afa8c70fb2d1740c54f574f1318daee
Alexander Ereshkin <xcode111@mail.ru>
2024-05-12 21:51:31 +0300

    Add price info in Selected Search screen.

--- app/src/main/java/ru/alexereh/tickets/presentation/selectedsearch/recycler/ticketsoffers/TicketsOffersViewHolder.kt
+++ app/src/main/java/ru/alexereh/tickets/presentation/selectedsearch/recycler/ticketsoffers/TicketsOffersViewHolder.kt
@@ -1,20 +1,29 @@
 package ru.alexereh.tickets.presentation.selectedsearch.recycler.ticketsoffers
 
+import android.annotation.SuppressLint
 import androidx.appcompat.content.res.AppCompatResources
 import androidx.recyclerview.widget.RecyclerView
 import ru.alexereh.tickets.R
 import ru.alexereh.tickets.databinding.CardTicketOfferBinding
 import ru.alexereh.tickets.domain.model.TicketsOfferModel
+import java.text.DecimalFormat
 
 class TicketsOffersViewHolder(
     private val binding: CardTicketOfferBinding
 ) : RecyclerView.ViewHolder(binding.root) {
+    private fun formatter(n: Int) =
+        DecimalFormat("#,###")
+            .format(n)
+            .replace(",", " ")
+
+    @SuppressLint("SetTextI18n")
     fun bind(model: TicketsOfferModel, position: Int) {
         with(binding) {
             timeRangeTv.text = model.timeRange.reduce { accumulator, current ->
                 accumulator + " " + current
             }
             titleTv.text = model.title
+            priceTv.text = formatter(model.price.value) + " ₽"
             when (position) {
                 0 -> {
                     directionIv.setImageDrawable(
